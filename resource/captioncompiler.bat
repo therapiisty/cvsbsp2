c4af2372e95ff497834c359e12018d5bb7a04737
therapiisty <pleb12343@gmail.com>
2024-02-11 15:33:29 -0500

    add caption tool, add dialogue test and update captions, add second version of the first level

---
+++ resource/captioncompiler.bat
@@ -0,0 +1,2 @@
+"F:\SteamLibrary\steamapps\common\Source SDK Base 2013 Singleplayer\bin\captioncompiler.exe" %1 -game "C:\Program Files (x86)\Steam\steamapps\sourcemods\cvsbsp_chapter2"
+pause
---
+++ resource/compiler.bat
@@ -0,0 +1,2 @@
+"Z:\SteamLibrary\steamapps\common\Half-Life 2\bin\captioncompiler.exe" -v "closecaption_english.txt"
+pause
